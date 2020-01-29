e6f355b3cff88aaf0d4ffbf4940a085250df304e
Ricardo Amaral <master@ricardoamaral.net>
2020-01-29 18:43:46 +0000

    test: add missing semantics unit tests

---
+++ tests/semantics.test.js
@@ -0,0 +1,19 @@
+const defaultSemanticsConfiguration = require('../semantics');
+
+describe('Default', () => {
+    describe('with semantics rules', () => {
+        test('configuration is exported correctly', () => {
+            expect(defaultSemanticsConfiguration).toEqual({
+                extends: [
+                    'plugin:@typescript-eslint/recommended',
+                    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+                    'plugin:prettier/recommended',
+                    'prettier/@typescript-eslint',
+                ],
+                parser: '@typescript-eslint/parser',
+                parserOptions: { project: './tsconfig.json' },
+                rules: { 'no-undef': 'off' },
+            });
+        });
+    });
+});
